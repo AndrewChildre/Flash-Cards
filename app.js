58d4d37fb6546a45eae36ee51a5e2087a0741eaa
andrew childre <andrewchildre@gmail.com>
2021-02-20 21:11:54 -0700

    commit sum

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello')
+
+const questionBox = document.querySelector('.question')
+const questionBtn = document.querySelector('.btn-1')
+const answerBtn = document.querySelector('.btn-2')
+const answerBox = document.querySelector('.answer')
+
+//data1 = Object.entries(content1)
+//console.log(data1)
+    content1 = 
+			{
+				question1: 'answer1',
+				question2: 'answer2',
+				question3: 'answer3',
+			},
+    
+            data1 = Object.entries(content1)
+    let randomQuestion = data1[Math.floor(Math.random() * data1.length)]
+    console.log(randomQuestion)   
+
+
+/questionBtn.addEventListener('click', () => {
+
+})
+answerBtn.addEventListener('click', () => { answerBox.style.display = 'block'
+    
+console.log('click2') })
